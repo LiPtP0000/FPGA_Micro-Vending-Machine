0ad874d147fada2772f6c43faa02983a4597b602
13649310743 <129015054+13649310743@users.noreply.github.com>
2024-09-03 09:56:22 +0800

    Add files via upload

--- display_design.v
+++ display_design.v
@@ -18,76 +18,94 @@
 // Additional Comments:
 // 
 //////////////////////////////////////////////////////////////////////////////////
-
-
-module display_design(
-//显示模块  
-input sys_clk, 
-input [7:0] need_money,//所需金额 
-input [7:0] input_money,//投币的总币值 
-input [7:0] change_money,//找出多余金额 
-output reg [7:0] bit_select,//数码管位选 
-output reg [7:0] seg_select//数码管段选 
-); 
- //分频器，将时钟信号频率降低用于扫描数码管
-reg [31:0] count_num = 32'd0;
-always @(posedge sys_clk)
-begin
- if(count_num == 32'd99_999)
- begin
- count_num <= 32'd0;
- end
- else begin
- count_num <=count_num + 1'd1;
- end
-end
-//循环扫描不同数码管
-reg [2:0] sig_num = 3'd0;
-always @(posedge sys_clk)
-begin
-if(count_num == 32'd99_999)
-begin
- if(sig_num == 3'd7)
- begin
- sig_num <= 3'd0;
- end
- else begin
- sig_num <= sig_num + 1'd1;
- end
-end
-end
-//给每一位确定输出的信息
-reg [3:0] display_num = 4'd0;
-always @(posedge sys_clk)
-begin
- case (sig_num)
- 3'd0: begin bit_select <= 8'b11111110; display_num <= need_money % 10; end
- 3'd1: begin bit_select <= 8'b11111101; display_num <= need_money / 10; end
- 3'd2: begin bit_select <= 8'b11111011; display_num <= 4'd10; end // 显示 "-"
- 3'd3: begin bit_select <= 8'b11110111; display_num <= input_money % 10; end
- 3'd4: begin bit_select <= 8'b11101111; display_num <= input_money / 10; end
- 3'd5: begin bit_select <= 8'b11011111; display_num <= 4'd10; end // 显示 "-"
- 3'd6: begin bit_select <= 8'b10111111; display_num <= change_money % 10; end
- 3'd7: begin bit_select <= 8'b01111111; display_num <= change_money / 10; end
-  default: bit_select <= 8'b11111111;
- endcase
-end
- //段选输出，分别定义显示0到9
- always @(posedge sys_clk)
- begin
-  case (display_num)
-  4'd0: seg_select <= 8'b1100_0000;
-  4'd1: seg_select <= 8'b1111_1001;
-  4'd2: seg_select <= 8'b1010_0100;
-  4'd3: seg_select <= 8'b1011_0000;
-  4'd4: seg_select <= 8'b1001_1001;
-  4'd5: seg_select <= 8'b1001_0010;
-  4'd6: seg_select <= 8'b1000_0010;
-  4'd7: seg_select <= 8'b1111_1000;
-  4'd8: seg_select <= 8'b1000_0000;
-  4'd9: seg_select <= 8'b1001_0000;
-  4'd10: seg_select <= 8'b1011_1111; // "-"
-   default: ;
-  endcase
- end
+//主要思路是动态循环显示
+module display_design(  
+    // 显示模块接口  
+    input sys_clk,   
+    input [7:0] need_money,         // 所需金额  
+    input [7:0] input_money,        // 投币的总币值  
+    input [7:0] change_money,       // 找出多余金额  
+    output reg [7:0] bit_select,    // 数码管位选  
+    output reg [7:0] seg_select     // 数码管段选  
+);  
+//定义16进制的显示编码
+parameter SEG_0 = 8'b1100_0000, SEG_1 = 8'b1111_1001, 
+            SEG_2 = 8'b1010_0100, SEG_3 = 8'b1011_0000, 
+            SEG_4 = 8'b1001_1001, SEG_5 = 8'b1001_0010, 
+            SEG_6 = 8'b1000_0010, SEG_7 = 8'b1111_1000, 
+            SEG_8 = 8'b1000_0000, SEG_9 = 8'b1001_0000, 
+            SEG_A = 8'b1000_1000, SEG_B = 8'b1000_0011, 
+            SEG_C = 8'b1100_0110, SEG_D = 8'b1010_0001, 
+            SEG_E = 8'b1000_0110, SEG_F = 8'b1000_1110,//16进制
+            SEG_S = 8'b1011_1111;//空格 
+// ------------------------------------------  
+// 分频器，将时钟信号频率降低用于扫描数码管  
+// ------------------------------------------  
+reg [31:0] count_num = 32'd0;  
+always @(posedge sys_clk) begin  
+    if (count_num == 32'd99_999) begin  
+        count_num <= 32'd0;  
+    end else begin  
+        count_num <= count_num + 1'd1;  
+    end  
+end  
+  
+// ------------------------------------------  
+// 循环扫描不同数码管  
+// ------------------------------------------  
+reg [2:0] sig_num = 3'd0;  
+always @(posedge sys_clk) begin  
+    if (count_num == 32'd99_999) begin  
+        if (sig_num == 3'd7) begin  
+            sig_num <= 3'd0;  
+        end else begin  
+            sig_num <= sig_num + 1'd1;  
+        end  
+    end  
+end  
+  
+// ------------------------------------------  
+// 数码管位选和显示数字逻辑  
+// ------------------------------------------  
+reg [3:0] display_num = 4'd0;  
+always @(posedge sys_clk) begin  
+    case (sig_num)  
+        3'd0: begin bit_select <= 8'b11111110; display_num <= need_money % 10; end  
+        3'd1: begin bit_select <= 8'b11111101; display_num <= need_money / 10; end  
+        3'd2: begin bit_select <= 8'b11111011; display_num <= 4'd16; end  // 显示 空格"-"  
+        3'd3: begin bit_select <= 8'b11110111; display_num <= input_money % 10; end  
+        3'd4: begin bit_select <= 8'b11101111; display_num <= input_money / 10; end  
+        3'd5: begin bit_select <= 8'b11011111; display_num <= 4'd16; end  // 显示 空格"-"  
+        3'd6: begin bit_select <= 8'b10111111; display_num <= change_money % 10; end  
+        3'd7: begin bit_select <= 8'b01111111; display_num <= change_money / 10; end  
+        default: bit_select <= 8'b11111111;  
+    endcase  
+end  
+  
+// ------------------------------------------  
+// 数码管段选输出  
+// ------------------------------------------  
+always @(posedge sys_clk) begin  
+    case (display_num)  
+        4'd0: seg_select <= SEG_0;  
+        4'd1: seg_select <= SEG_1;  
+        4'd2: seg_select <= SEG_2;  
+        4'd3: seg_select <= SEG_3;  
+        4'd4: seg_select <= SEG_4;  
+        4'd5: seg_select <= SEG_5;  
+        4'd6: seg_select <= SEG_6;  
+        4'd7: seg_select <= SEG_7;  
+        4'd8: seg_select <= SEG_8;  
+        4'd9: seg_select <= SEG_9;  
+        4'd10: seg_select <= SEG_A;  
+        4'd11: seg_select <= SEG_B;
+        4'd12: seg_select <= SEG_C;
+        4'd13: seg_select <= SEG_D;
+        4'd14: seg_select <= SEG_E;
+        4'd15: seg_select <= SEG_F;
+        4'd16: seg_select <= SEG_S;
+        default: ;  
+    endcase  
+end  
+  
 endmodule
